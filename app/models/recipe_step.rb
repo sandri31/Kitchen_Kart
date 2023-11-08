22f61c743588abd030912f35ab6fa2047327caff
Florent S <sandri822@hotmail.fr>
2023-11-08 22:58:05 +0100

    Add frozen string literal and update RecipeStep model

--- app/models/recipe_step.rb
+++ app/models/recipe_step.rb
@@ -1,3 +1,8 @@
+# frozen_string_literal: true
+
+# This is the model is used to describe the different steps required
+# to prepare a recipe. Each RecipeStep belongs to a specific Recipe.
+# Example: step 1: cut the onions, step 2: fry the onions, etc.
 class RecipeStep < ApplicationRecord
   belongs_to :recipe
 end
