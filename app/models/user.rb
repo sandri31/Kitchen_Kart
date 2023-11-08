0007df65965b99f8bdf1df528edf11f52f6dfb65
Florent S <sandri822@hotmail.fr>
2023-11-08 17:05:47 +0100

    🔧 Add associations, validations, and default role to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_one :shopping_bag, dependent: :destroy
+  has_many :recipes, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :ratings, dependent: :destroy
+
+  enum role: { user: 0, moderator: 1, admin: 2 }
+
+  validates :username, presence: true, uniqueness: { case_sensitive: false }
+
+  after_initialize :set_default_role, if: :new_record?
+
+  def set_default_role
+    self.role ||= :user
+  end
 end
