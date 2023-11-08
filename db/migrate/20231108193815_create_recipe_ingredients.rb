dd68888af7190aa77c4b38cdbbb92a8eac1c5148
Florent S <sandri822@hotmail.fr>
2023-11-08 20:42:46 +0100

    🔧 Create model for RecipeIngredient

---
+++ db/migrate/20231108193815_create_recipe_ingredients.rb
@@ -0,0 +1,12 @@
+class CreateRecipeIngredients < ActiveRecord::Migration[7.1]
+  def change
+    create_table :recipe_ingredients do |t|
+      t.references :recipe, null: false, foreign_key: true
+      t.references :ingredient, null: false, foreign_key: true
+      t.decimal :quantity, null: false
+      t.string :unit, null: false
+
+      t.timestamps
+    end
+  end
+end
