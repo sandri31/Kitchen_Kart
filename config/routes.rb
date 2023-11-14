f0e49eb55039bc4748ef8d7c8ad3e9b281b47da1
Florent S <sandri822@hotmail.fr>
2023-11-14 07:03:38 +0100

    🚧 Update recipe form and routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,8 @@ Rails.application.routes.draw do
       post :edit
     end
   end
-  resources :recipes
+
+  resources :recipes, path: 'recettes', path_names: { new: 'nouvelle_recette', edit: 'modifier_recette' }
   devise_for :users
 
   get 'up' => 'rails/health#show', as: :rails_health_check
